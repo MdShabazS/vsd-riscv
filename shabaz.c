9b04af9bcde8fbd3ece1a8a0fe06e31bf34fc56c
MOHAMMED SHABAZ S <128482516+MdShabazS@users.noreply.github.com>
2025-03-25 22:08:28 +0530

    Task 1 Updated

---
+++ shabaz.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int sum=0,i,n;
+printf("Enter the value of n=");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+  sum=sum+i;
+}
+printf("The sum of number %d \n",sum);
+}
